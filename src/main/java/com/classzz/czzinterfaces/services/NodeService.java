d0fa0b6356774a9651c2d62ae3f24f41304ba74d
liujiawei <13293920099@163.com>
2020-06-07 23:19:57 +0800

    Add the getAddressRanking query interface1/2

--- src/main/java/com/classzz/czzinterfaces/services/NodeService.java
+++ src/main/java/com/classzz/czzinterfaces/services/NodeService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class NodeService {
@@ -19,15 +20,20 @@ public class NodeService {
     private static final Logger LOGGER = LoggerFactory.getLogger(NodeService.class);
 
     /**
-     * Get block difficulty information
+     * @funution Get ...
+     * @author Robin-byte
+     * @time 2020/06/07 16:06
      * @return
      */
-    public MiningInfoDto getMiningInfo(){
+    public static List<Map<String, String>> tempMethodName(Map map){
+        List<Map<String, String>> resultList = new ArrayList<>();
+
         List<Object> params = new ArrayList<>();
 
         JSONObject object = HttpClient.GetResult(params,"getmininginfo");
-        MiningInfoDto miningInfoDto = JSON.toJavaObject(object.getJSONObject("result"),MiningInfoDto.class) ;
-        return miningInfoDto;
+        Map resultMap = JSON.toJavaObject(object.getJSONObject("result"), Map.class);
+
+        return resultList;
     }
 
     /**
