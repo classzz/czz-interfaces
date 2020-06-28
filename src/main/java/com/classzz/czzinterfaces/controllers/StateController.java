a694e5a1886a10895e32d50091a9290a1d4f6891
Administrator <1433929466@qq.com>
2020-06-28 16:49:57 +0800

    Modify the amount of pledge deposit except (10^8)

--- src/main/java/com/classzz/czzinterfaces/controllers/StateController.java
+++ src/main/java/com/classzz/czzinterfaces/controllers/StateController.java
@@ -1,5 +1,6 @@
 package com.classzz.czzinterfaces.controllers;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.classzz.czzinterfaces.dtos.node.MiningInfoDto;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 @Controller
@@ -67,7 +69,7 @@ public class StateController {
             result.put("data", data);
             return result;
         }
-
+        //质押金/(10^8)
         try {
             List<StateInfoDto> stateInfo = stateService.getStateInfo();
             if (null != stateInfo && stateInfo.size() > 0) {
@@ -77,13 +79,18 @@ public class StateController {
                         data.put("id", stateInfo.get(i).getExchange_id());
                         data.put("address", stateInfo.get(i).getAddress());
                         data.put("beacon_address", stateInfo.get(i).getToAddress_pk_hex());
-                        data.put("staking_amount", stateInfo.get(i).getStaking_amount());
+
+                        if(null!=stateInfo.get(i).getStaking_amount()&&stateInfo.get(i).getStaking_amount()>0) {
+                            data.put("staking_amount", stateInfo.get(i).getStaking_amount()/ Math.pow(10,8));
+                        }else{
+                            data.put("staking_amount", stateInfo.get(i).getStaking_amount());
+                        }
+
                         data.put("asset_flag", stateInfo.get(i).getAsset_flag());
                         data.put("fee", stateInfo.get(i).getFee());
                         data.put("keep_time", stateInfo.get(i).getKeep_time());
                         data.put("white_list", stateInfo.get(i).getWhite_list());
                         data.put("CoinBaseAddress", stateInfo.get(i).getCoinBaseAddress());
-//                        array.add(data);
                         result.put("data", data);
                     }
                 }
@@ -97,4 +104,5 @@ public class StateController {
         }
         return result;
     }
+
 }
