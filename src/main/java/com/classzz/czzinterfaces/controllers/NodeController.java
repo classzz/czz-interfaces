f972f50735d5c44619da966dfb776cf2a4ddc23f
Baker-Martin <Baker-Martin@protonmail.com>
2021-02-23 23:04:54 +0800

    update getAddressRanking params

--- src/main/java/com/classzz/czzinterfaces/controllers/NodeController.java
+++ src/main/java/com/classzz/czzinterfaces/controllers/NodeController.java
@@ -1,7 +1,6 @@
 package com.classzz.czzinterfaces.controllers;
 
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.classzz.czzinterfaces.dtos.node.MiningInfoDto;
 import com.classzz.czzinterfaces.services.NodeService;
@@ -9,9 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
